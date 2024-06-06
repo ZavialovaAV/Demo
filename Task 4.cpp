4680d6e207fcf930ac9296f887bf7249e8f34708
Завьялова Александра Валерьевна ТСА-141 <160851697+ZavialovaAV@users.noreply.github.com>
2024-06-06 19:53:46 +0300

    Update Task 4.cpp

--- Task 4.cpp
+++ Task 4.cpp
@@ -34,14 +34,21 @@ int main() {
     cout << " x       | y" << endl;
     cout << "----------------" << endl;
 
-     for (double x = x_start; x <= x_end; x += delta_x) 
-     {
-        double y = computeY(x);
-        cout << fixed << setprecision(4) << x << " | " << y << endl; 
+    for (double x = x_start; x <= x_end; x += delta_x) 
+    {
+        if (isInDomain(x)) 
+        {
+            double y = computeY(x); 
+            cout << fixed << setprecision(4) << x << " | " << y << endl; 
+        } else {
+            cout << fixed << setprecision(4) << x << " | Вне области определения" << endl; 
+            break;
+        }
     }
 
     return 0;
 }
+
 
 double getValue() {
     double value;
